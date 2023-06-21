6a5355b21ce646e98db6e591bb7d07cc6adc1360
samuel <samzerr7@gmail.com>
2023-06-21 16:10:39 -0500

    service discovery added

---
+++ discoveryserver/src/test/java/edu/miu/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- inventory-service/src/main/java/edu/miu/inventoryservice/model/Inventory.java
+++ inventory-service/src/main/java/edu/miu/inventoryservice/model/Inventory.java
@@ -1,10 +1,7 @@
 package edu.miu.inventoryservice.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +17,9 @@ public class Inventory {
 
     @Id
     @GeneratedValue
+
     private Long id;
+
     private String skuCode;
     private Integer quantity;
 }
